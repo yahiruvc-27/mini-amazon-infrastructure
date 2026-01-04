f22101f51997bc6722eea2d97cb8787bd97f9b7a
yairuvc-27 <yairuvc@gmail.com>
2026-01-04 15:32:31 -0600

    WEB user data script indempotent

--- app-user-data.sh
+++ app-user-data.sh
@@ -6,19 +6,19 @@ set -euxo pipefail  # Better debugging (print commands, exit on error)
 dnf update -y
 dnf install -y python3-pip git mariadb105 
 
-# 2. Go to ec2-user home
+# 2. Change directory to ec2-user home
 cd /home/ec2-user
 
-# 3. Clone your GitHub repo (ensure public or use HTTPS token)
+# 3. Clone your GitHub repo 
 git clone https://github.com/yahiruvc-27/mini-amazon-backend.git mini-amazon-app
 cd mini-amazon-app
 
-# 4. Install Python packages
+# 4. Install Python packages from txt file
 pip3 install -r requirements.txt
 
-# 5. Create environment file with your real values (temporary)
+# 5. Create environment file with variable real values (modify here, secrets*)
 cat <<EOF > /etc/mini-amazon.env
-RDS_ENDPOINT=miniamazondb.cctm6is0819y.us-east-1.rds.amazonaws.com
+RDS_ENDPOINT=mini-amazondb.cctm6is0819y.us-east-1.rds.amazonaws.com
 DB_USER=admin
 DB_PASS=Vicaya27#6
 DB_NAME=store
@@ -26,16 +26,16 @@ AWS_REGION=us-east-1
 SES_SOURCE=yahiruvc@gmail.com
 EOF
 
-# 6. Fix permissions
+# 6. Fix permissions recursively
 chown -R ec2-user:ec2-user /home/ec2-user/mini-amazon-app
 
-# 7. Move systemd service
+# 7. Move systemd file service to "/etc/systemd/system/mini-amazon.service"
 mv /home/ec2-user/mini-amazon-app/mini-amazon.service /etc/systemd/system/mini-amazon.service
 
-# 8. Add env file reference
+# 8. Add env file reference, so that mini amazon can use this variables
 sed -i '/\[Service\]/a EnvironmentFile=/etc/mini-amazon.env' /etc/systemd/system/mini-amazon.service
 
 # 9. Reload and enable service
-systemctl daemon-reload
-systemctl enable --now mini-amazon
+systemctl daemon-reload # daemon created but must be loaded (mini-amazon.service)
+systemctl enable --now mini-amazon # enable and start
 systemctl status mini-amazon -l --no-pager
--- web-user-data.sh
+++ web-user-data.sh
@@ -1,53 +1,101 @@
 #!/bin/bash
+# ============================================================
+# Web Tier EC2 User Data Script (Idempotent)
+# This script is safe to re-run 
+# ============================================================
+
 set -euxo pipefail
 
-# === 1.- Update and Install packages ===
+# === 1. Update and install required packages ===
+
+# dnf is idempotent: re-running will not reinstall packages
 dnf update -y
 dnf install -y nginx git awscli
 
-# === 2.- Cretate variables ===
-export S3_BUCKET="mini-amazon-bucket-yahir"
-# modify with the backend DNS or IP
-export IP_BACKEND="10.0.1.162"
-export WEBROOT="/var/www/mini-amazon"
-export NGINX_CONFIG_PATH="/etc/nginx/conf.d"
+# === 2. Define variables used === 
 
-# === 3.- Prepare root directory for GitHub code ===
+# NOTE: MUST REPLACE S3_BUCKET IP_BACKEND -> real values
+S3_BUCKET=""
+IP_BACKEND=""
+
+# Do not modidy
+WEBROOT="/var/www/mini-amazon"
+NGINX_CONFIG_PATH="/etc/nginx/conf.d"
+
+# === 3. Ensure base directories exist ===
+
 mkdir -p /var/www
 
-# === 4.- Clone GitHub to root dir ====
-git clone https://github.com/yahiruvc-27/mini-amazon-frontend.git ${WEBROOT}
-cd ${WEBROOT}
+# 4. Clone or update frontend / WEB GitHub repository (IDEMPOTENT)
 
-sed -i "s|BACKEND_PLACEHOLDER|${IP_BACKEND}|g" ${WEBROOT}/nginx/mini-amazon.conf
+if [ ! -d "${WEBROOT}/.git" ]; then # if there is no local repo clone
+  echo "Cloning frontend repository..."
+  git clone https://github.com/yahiruvc-27/mini-amazon-frontend.git "${WEBROOT}" # clone to the desired path ${WEBROOT}
+else # just update or pull
+  echo "Frontend repository already exists. Pulling latest changes..."
+  cd "${WEBROOT}"
+  git pull
+fi
 
 
-# === 5.- Move nginx config adn clean the default
-mv ${WEBROOT}/nginx/mini-amazon.conf ${NGINX_CONFIG_PATH}/mini-amazon.conf
+# 5. Config file for nginx -> get backend endpoint 
+
+NGINX_SOURCE_CONF="${WEBROOT}/nginx/mini-amazon.conf" # NGINX config location from Git clone
+NGINX_TARGET_CONF="${NGINX_CONFIG_PATH}/mini-amazon.conf" # NGINX config DESIRED destination (move)
+
+# Replace backend placeholder to the IP only if it still exists
+
+# IF "BACKEND_PLACEHOLDER" IN "${NGINX_SOURCE_CONF}" -> repace placehoder for Backend IP
+if grep -q "BACKEND_PLACEHOLDER" "${NGINX_SOURCE_CONF}"; then
+    # sed = "search and replace |this|for that|"
+    # -i -> save the file after the replace
+    # s ->start, g -> global -> all the appearances
+  sed -i "s|BACKEND_PLACEHOLDER|${IP_BACKEND}|g" "${NGINX_SOURCE_CONF}"
+fi
+
+# Copy nginx config only if not already in destination folder
+# IF FILE X not exist then
+if [ ! -f "${NGINX_TARGET_CONF}" ]; then
+  echo "Installing nginx configuration..."
+  cp "${NGINX_SOURCE_CONF}" "${NGINX_TARGET_CONF}"
+fi
+
+# Remove default nginx config, just to be safe
 rm -f /etc/nginx/conf.d/default.conf
 
-# === 6.- Create az.txt with instance metadata ===
 
-# Request a session token 
+# === 6. Write availability zone metadata ===
+
+# Request  token
 TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" \
   -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
 
-# Use the token to get the Availability Zone
-AZ=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" \
+# Fetch availability zone
+AZ=$(curl -H "X-aws-ec2-metadata-token: ${TOKEN}" \
   -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
 
-# Save it to root frontend directory and fix permissions
-echo "$AZ" > ${WEBROOT}/az.txt
-chmod 644 ${WEBROOT/}/az.txt
+# Write the ${AZ} to a file (overwrite)
+echo "${AZ}" > "${WEBROOT}/az.txt"
+chmod 644 "${WEBROOT}/az.txt"
 
-# === 7.- Get S3 product images to image directory ====
-cd ${WEBROOT}/images
-aws s3 sync s3://$S3_BUCKET ${WEBROOT}/images --delete
+# === 7 Sync product images from S3 ===
 
-# === 8.- Permissions and nginx service start ===
-chown -R nginx:nginx ${WEBROOT}
-chmod -R 755 ${WEBROOT}
+# what is sync all images from S3
+# download image if not exist (in EC2 dir), delete (from EC2 dir) if they are no longer in s3
+mkdir -p "${WEBROOT}/images"
+aws s3 sync "s3://${S3_BUCKET}" "${WEBROOT}/images" --delete
+
+
+# === 8. Fix ownership /permissions for nginx ===
+# there is an NGINX user -> the service must own the files
+
+chown -R nginx:nginx "${WEBROOT}"
+chmod -R 755 "${WEBROOT}"
+
+# === 9. Enable and start nginx ===
 
 systemctl daemon-reload
 systemctl enable --now nginx
-systemctl status nginx -l --no-pager
+
+# Oheck if NGINX is working
+systemctl is-active nginx || systemctl status nginx -l --no-pager
